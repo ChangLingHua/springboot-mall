ef7178180e14fd24f4eb69d4b93e09c6c61a895c
ChangLingHua <qwwe092336@gmail.com>
2023-09-28 14:40:49 +0800

    商品功能 - Refactor category，使用 Enum 類型

--- springboot-mall/src/main/java/com/hua/springbootmall/rawmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/hua/springbootmall/rawmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.hua.springbootmall.rawmapper;
 
+import com.hua.springbootmall.constant.ProductCategory;
 import com.hua.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,14 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProduct_id(resultSet.getInt("product_id"));
         product.setProduct_name(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        // 得到從資料庫取出的數據
+        String categoryStr = resultSet.getString("category");
+        // 將字串轉換成 Enum 類型
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+        //product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImage_url(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
